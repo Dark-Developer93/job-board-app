7edef38bf90a7fc60b601811a201b89c4d1ed49d
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-09-28 16:14:31 +0300

    chore: Add job types and location types to lib/job-types.ts

---
+++ lib/prisma.ts
@@ -0,0 +1,19 @@
+/* eslint-disable vars-on-top */
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
