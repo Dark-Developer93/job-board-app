ab6bbba6fc31318f4c93c5d2daeb364a6795f41e
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-09-28 16:14:56 +0300

    chore: Add lazy loading for remote images in next.config.mjs and update dependencies in package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "logo.clearbit.com",
+      },
+      {
+        protocol: "https",
+        hostname: "yt3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "smartdiary.co",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
