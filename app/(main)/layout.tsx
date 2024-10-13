00e881f861e5476336845a91da57400926b0ab3e
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-10-13 14:10:06 +0300

    feat: Update RootLayout to include Toaster and FullFooter components

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,17 +2,23 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import "@/app/globals.css";
-import Navbar from "@/components/navbar/Navbar";
-import Footer from "@/components/footer/Footer";
+import { Toaster } from "@/components/ui/toaster";
+import { MinimalNavbar } from "@/components/navbar/Navbar";
+import FullFooter from "@/components/footer/FullFooter";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "JobBoard",
+  title: {
+    template: "%s | JobBoard",
+    default: "JobBoard",
+  },
   description:
-    "JobBoard: Your Intelligent Job Board Companion. Streamline your job search with AI-powered features including smart job suggestions, seamless adding and deleting of jobs, robust authentication, and efficient search functionality. Stay organized and boost your productivity with our intuitive job board app.",
+    "JobBoard: Your Intelligent Job Board Companion. Streamline your job search, seamless adding and deleting of jobs, robust authentication, and efficient search functionality. Stay organized and boost your productivity with our intuitive job board app.",
+  // "JobBoard: Your Intelligent Job Board Companion. Streamline your job search with AI-powered features including smart job suggestions, seamless adding and deleting of jobs, robust authentication, and efficient search functionality. Stay organized and boost your productivity with our intuitive job board app.",
   keywords:
     "JobBoard, job board, job search, AI-powered, smart suggestions, productivity, authentication, search functionality, nextjs, typescript",
+  // "JobBoard, job board, job search, AI-powered, smart suggestions, productivity, authentication, search functionality, nextjs, typescript",
   // TODO: add icons
   // icons: {
   //   icon: "/favicon.ico",
@@ -23,17 +29,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-  isAuthenticated,
 }: {
   children: React.ReactNode;
-  isAuthenticated?: boolean;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} min-w-[350px] bg-slate-50`}>
-        <Navbar isAuthenticated={isAuthenticated} />
+      <body className={`${inter.className} min-w-[350px]`}>
+        {/* Add the isAuthenticated and isAdmin props to the MinimalNavbar */}
+        <MinimalNavbar />
         {children}
-        <Footer />
+        <Toaster />
+        <FullFooter />
       </body>
     </html>
   );
