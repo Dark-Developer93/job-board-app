e085590a8f589fb2773d30dc57dd7ac1db85ac27
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-10-13 11:48:23 +0300

    feat: Create LoginForm component for user login

---
+++ components/login-form/LoginForm.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+// import Link from "next/link";
+// import { signInAction } from "@/app/actions/auth-action";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+
+export const description =
+  "A login form with email and password. There's an option to login with Google and a link to sign up if you don't have an account.";
+
+export function LoginForm() {
+  const handleGoogleLogin = async () => {
+    console.log("Google login");
+    // await signInAction();
+  };
+
+  return (
+    <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl">Login</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="password">Password</Label>
+              <Link
+                href="/forgot-password"
+                className="ml-auto inline-block text-sm underline"
+              >
+                Forgot your password?
+              </Link>
+            </div>
+            <Input id="password" type="password" required />
+          </div>
+          <Button type="submit" className="w-full" disabled>
+            Login
+          </Button>
+          <Button
+            variant="outline"
+            className="w-full"
+            onClick={handleGoogleLogin}
+          >
+            Login with Google
+          </Button>
+        </div>
+        <div className="mt-4 text-center text-sm">
+          Does not have an account?{" "}
+          <Link href="/auth/signup" className="underline">
+            Sign up
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
