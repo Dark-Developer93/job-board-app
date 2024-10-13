8093c25fd94ff5654d6a13f821da617ac8f464d2
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-10-13 11:48:39 +0300

    feat: Create login page with LoginForm component

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "@/components/login-form/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <LoginForm />
+    </div>
+  );
+}
