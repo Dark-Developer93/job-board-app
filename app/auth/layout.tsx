8fdf7228f60765d92ec022980d7a4aa9b1b3360c
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-10-13 12:52:36 +0300

    feat: Add RootLayout component for wrapping the entire application

---
+++ app/auth/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+// eslint-disable-next-line camelcase
+import { Inter } from "next/font/google";
+
+import "@/app/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "JobBoard",
+  description:
+    "JobBoard: Your Intelligent Job Board Companion. Streamline your job search with AI-powered features including smart job suggestions, seamless adding and deleting of jobs, robust authentication, and efficient search functionality. Stay organized and boost your productivity with our intuitive job board app.",
+  keywords:
+    "JobBoard, job board, job search, AI-powered, smart suggestions, productivity, authentication, search functionality, nextjs, typescript",
+  // TODO: add icons
+  // icons: {
+  //   icon: "/favicon.ico",
+  //   shortcut: "/favicon-16x16.png",
+  //   apple: "/apple-touch-icon.png",
+  // },
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="flex min-h-screen flex-col">
+          <main className="flex-1">{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
