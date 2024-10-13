2d2798c3bf85249ea836b4c27206860b4e477120
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-10-13 11:48:48 +0300

    feat: Create signup page with SignupForm component

---
+++ app/auth/signup/page.tsx
@@ -0,0 +1,9 @@
+import SignupForm from "@/components/signup-form/SignupForm";
+
+export default function SignupPage() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <SignupForm />
+    </div>
+  );
+}
